c98aa7344cb4e3527e09768581bd7dc295e014d9
Debora Bimbi <deborabimbi1985@gmail.com>
2017-04-20 14:35:35 +0200

    added host and port

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      #super.merge(Host:  '10.90.0.40', Port: 4001)
+      super.merge(Host:  '0.0.0.0', Port: 4001)
+    end
+  end
+end
